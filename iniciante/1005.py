bd1a13962c6fca3f0c79fe31eaeccb7f9e54e141
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-25 10:32:47 -0300

    first commit of beecrowd platform

---
+++ iniciante/1005.py
@@ -0,0 +1,4 @@
+one_note=float(input())
+two_note=float(input())
+average=((one_note*3.5)+(two_note*7.5))/(3.5+7.5)
+print("MEDIA = %.5f" %average)
---
+++ iniciante/1006.py
@@ -0,0 +1,5 @@
+A=float(input())
+B=float(input())
+C=float(input())
+notes_average=((A*2.0)+(B*3.0)+(C*5.0))/(2.0+3.0+5.0)
+print("MEDIA = %.1f" %notes_average)
