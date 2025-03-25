e6b7b9f83a29f7d593edd9acc56edd78b7e2e47d
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-25 10:48:45 -0300

    calculate salary brute

---
+++ iniciante/1008.py
@@ -0,0 +1,6 @@
+number=int(input())
+hours_worked=int(input())
+value_hours_worked=float(input())
+salary=hours_worked*value_hours_worked
+print("NUMBER =",number)
+print("SALARY = U$ %.2f" %salary)
